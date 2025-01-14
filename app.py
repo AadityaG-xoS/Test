4c70a933d2e53937c5bfb526d2dff8b24c94529a
AadityaGawali <97809690+AadityaGawali@users.noreply.github.com>
2025-01-14 16:28:38 +0530

    Your commit message

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ def home():
     return render_template('index.html', reviews=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
